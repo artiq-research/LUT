6cbf3f936a04985ff2baab899ca61ae9c5ade46f
김영한 <godppkyh@gmail.com>
2020-09-17 15:18:48 +0900

    Light 코드 1차 완성

--- lut/light.py
+++ lut/light.py
@@ -16,5 +16,43 @@ def contrast(img, clipLimit, gridsize):
     res = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
     return res
 
+def automatic_brightness_and_contrast(image, clip_hist_percent=1):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Calculate grayscale histogram
+    hist = cv2.calcHist([gray],[0],None,[256],[0,256])
+    hist_size = len(hist)
+
+    # Calculate cumulative distribution from the histogram
+    accumulator = []
+    accumulator.append(float(hist[0]))
+    for index in range(1, hist_size):
+        accumulator.append(accumulator[index -1] + float(hist[index]))
+
+    # Locate points to clip
+    maximum = accumulator[-1]
+    clip_hist_percent *= (maximum/100.0)
+    clip_hist_percent /= 2.0
+
+    # Locate left cut
+    minimum_gray = 0
+    while accumulator[minimum_gray] < clip_hist_percent:
+        minimum_gray += 1
+
+    # Locate right cut
+    maximum_gray = hist_size -1
+    while accumulator[maximum_gray] >= (maximum - clip_hist_percent):
+        maximum_gray -= 1
+
+    # Calculate alpha and beta values
+    alpha = 255 / (maximum_gray - minimum_gray)
+    beta = -minimum_gray * alpha
+
+    auto_result = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
+    return (auto_result, alpha, beta)
+
 if __name__ == "__main__":
-    pass
+    image = cv2.imread('img/50343084823_48a0274a6a_c.jpg')
+    auto_result, alpha, beta = automatic_brightness_and_contrast(image)
+    cv2.imshow('auto_result', auto_result)
+    cv2.waitKey()

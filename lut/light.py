f49bc3005f090102953f56aceb6c6df8a7df8b94
김영한 <godppkyh@gmail.com>
2020-09-17 14:47:37 +0900

    Add contrast editing function

--- lut/light.py
+++ lut/light.py
@@ -7,6 +7,14 @@ def exposure(img, gamma):
     gamma_table=np.round(np.array(gamma_table)).astype(np.uint8)
     return cv2.LUT(img, gamma_table)
 
+def contrast(img, clipLimit, gridsize):
+    lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    lab_planes = cv2.split(lab)
+    clahe = cv2.createCLAHE(clipLimit=clipLimit,tileGridSize=(gridsize,gridsize))
+    lab_planes[0] = clahe.apply(lab_planes[0])
+    lab = cv2.merge(lab_planes)
+    res = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+    return res
 
 if __name__ == "__main__":
     pass
